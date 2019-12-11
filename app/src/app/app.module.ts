7f1e9607249911110ae5d6a9ebb8b457546097c6
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-12-11 22:16:46 +0530

    Rearranged the code base

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER} from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { HomeComponent } from './modules/home/home.component';
-import { GapiSession } from '../sessions/gapi.session'
+import { GooglePickerComponent } from './shared/google-picker/google-picker.component'
 
-export function initGapi(gapiSession: GapiSession) {
-  return () => gapiSession.initClient();
-}
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    GooglePickerComponent
   ],
   imports: [
     BrowserModule,
@@ -25,10 +22,7 @@ export function initGapi(gapiSession: GapiSession) {
     BrowserAnimationsModule,
     MatToolbarModule
   ],
-  providers: [
-    { provide: APP_INITIALIZER, useFactory: initGapi, deps: [GapiSession], multi: true },
-    GapiSession,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
---
+++ app/src/app/shared/google-picker/google-picker.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { keys } from '../../../keys'
+declare var gapi: any;
+declare var google: any;
+
+@Component({
+  selector: 'app-google-picker',
+  templateUrl: './google-picker.component.html',
+  styleUrls: ['./google-picker.component.css']
+})
+export class GooglePickerComponent {
+
+  developerKey = keys.ApiKey;
+  clientId = keys.ClientId
+  scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/drive'//insert scope here
+  ].join(' ');
+  pickerApiLoaded = false;
+  oauthToken?: any;
+
+  loadGoogleDrive() {
+    gapi.load('auth', { 'callback': this.onAuthApiLoad.bind(this) });
+    gapi.load('picker', { 'callback': this.onPickerApiLoad.bind(this) });
+  }
+
+  onAuthApiLoad() {
+    gapi.auth.authorize(
+      {
+        'client_id': this.clientId,
+        'scope': this.scope,
+        'immediate': false
+      },
+      this.handleAuthResult);
+  }
+
+  onPickerApiLoad() {
+    this.pickerApiLoaded = true;
+  }
+
+  handleAuthResult(authResult) {
+    let src;
+    if (authResult && !authResult.error) {
+      if (authResult.access_token) {
+        let view = new google.picker.View(google.picker.ViewId.DOCS);
+        view.setMimeTypes("application/vnd.google-apps.folder,image/png,image/jpeg,image/jpg,video/mp4");
+        let pickerBuilder = new google.picker.PickerBuilder();
+        let picker = pickerBuilder.
+          enableFeature(google.picker.Feature.SUPPORT_DRIVES).
+          enableFeature(google.picker.Feature.MULTISELECT_ENABLED).
+          setOAuthToken(authResult.access_token).
+          addView(view).
+          addView(new google.picker.DocsUploadView()).
+          setCallback(function (e) {
+            if (e[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+              let doc = e[google.picker.Response.DOCUMENTS][0];
+              src = doc[google.picker.Document.URL];
+              console.log("Document selected is", doc, "and URL is ", src)
+            }
+          }).
+          build();
+        picker.setVisible(true);
+      }
+    }
+  }
+
+}
